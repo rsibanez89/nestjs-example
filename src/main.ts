29aae55b986c5768f4ae6873ef1ec8c2b1d32a76
rsibanez89 <rsibanez89@gmail.com>
2020-06-09 19:56:01 +1000

    3 - Added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import config from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Nest example')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
   
   await app.listen(config.port, () => {
-    console.log(`Server started at http://localhost:${config.port}`);
+    console.log(`Swagger started at http://localhost:${config.port}/swagger`);
   });
 }
 bootstrap();
