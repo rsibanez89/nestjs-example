d7dca8defccaf5ffbd544bce188c12ffdd378db6
Rodrigo Ibanez <rsibanez89@gmail.com>
2020-06-10 12:54:42 +1000

    8 - Added authentication guard.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import config from './config';
 import { HttpLoggerMiddleware } from './common/http-logger.middleware';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [() => config], isGlobal: true }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
